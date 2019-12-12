2dd285e87af2e319c6401c839970f68bc2e832fe
JKS <juskus0310@naver.com>
2019-12-12 15:23:27 +0900

    naver,google 검색

--- hello.py
+++ hello.py
@@ -15,8 +15,17 @@ def pong():
     #request.args => get방식으로 데이터가 들어올 때.
     #request.form => post방식으로 데이터가 들어올 때.
     #print(request.form.get('keyword'))
+    #keyword=request.args.get('keyword')
     keyword=request.form.get('keyword')
     return render_template('pong.html',keyword=keyword)
+
+@app.route('/naver')
+def naver():
+    return render_template('naver.html')
+
+@app.route('/google')
+def google():
+    return render_template('google.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
